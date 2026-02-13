f8eb7150e921dfeee9c685a0683f94ec81a6c6f3
Dan Magnusson <pastadiablo@gmail.com>
2026-02-13 15:51:28 -0800

    Update to version 3.0.10 of CTV

--- assets/count_the_voids/alpha3/index.service.worker.js
+++ assets/count_the_voids/alpha3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770968837|111937197396';
+const CACHE_VERSION = '1771026433|169530398821';
 /** @type {string} */
 const CACHE_PREFIX = 'BlackCatJam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
