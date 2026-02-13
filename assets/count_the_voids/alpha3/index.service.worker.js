9fa8c8bf2fd04fd105ed08693282366b34ba5d5d
Dan Magnusson <pastadiablo@gmail.com>
2026-02-12 23:49:20 -0800

    Alpha 3.0.6 push - fixed music locally

--- assets/count_the_voids/alpha3/index.service.worker.js
+++ assets/count_the_voids/alpha3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770964519|107619094102';
+const CACHE_VERSION = '1770968837|111937197396';
 /** @type {string} */
 const CACHE_PREFIX = 'BlackCatJam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
