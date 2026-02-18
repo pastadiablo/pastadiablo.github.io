69433783d75cac2bf3256c4f6798adf2c6b08fd9
Dan Magnusson <pastadiablo@gmail.com>
2026-02-17 22:44:37 -0800

    Alpha 3.0.16

--- assets/count_the_voids/alpha3/index.service.worker.js
+++ assets/count_the_voids/alpha3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771395702|103458322549';
+const CACHE_VERSION = '1771397059|104814887413';
 /** @type {string} */
 const CACHE_PREFIX = 'BlackCatJam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
