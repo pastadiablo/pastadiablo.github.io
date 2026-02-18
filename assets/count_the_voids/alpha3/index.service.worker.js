712605284de1fabcc6979271ee3e3872bbb8f56e
Dan Magnusson <pastadiablo@gmail.com>
2026-02-18 00:26:34 -0800

    Alpah 3.0.18

--- assets/count_the_voids/alpha3/index.service.worker.js
+++ assets/count_the_voids/alpha3/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771401724|1138948843';
+const CACHE_VERSION = '1771403159|2574062531';
 /** @type {string} */
 const CACHE_PREFIX = 'BlackCatJam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
