d28aa9405687c7c0185e103a64e1b4ec90823119
Dan Magnusson <pastadiablo@gmail.com>
2026-03-11 16:22:24 -0700

    Re-attempting to upload alpha 4.0 along with changes to get github pages working again

--- assets/count_the_voids/alpha4/index.service.worker.js
+++ assets/count_the_voids/alpha4/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772751360|7854888977';
+const CACHE_VERSION = '1773270758|13664563125';
 /** @type {string} */
 const CACHE_PREFIX = 'BlackCatJam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
