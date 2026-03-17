4a549fa0828adbb382432778b6ff76553414b56a
Dan Magnusson <pastadiablo@gmail.com>
2026-03-16 17:05:02 -0700

    CTV Alpha 4.0.5

--- assets/count_the_voids/alpha4/index.service.worker.js
+++ assets/count_the_voids/alpha4/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773646251|28652854199';
+const CACHE_VERSION = '1773705889|88288275258';
 /** @type {string} */
 const CACHE_PREFIX = 'BlackCatJam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
